65139eaff72b489369f557babee0f7c4e2678c52
franklin rodriguez <franklinserif@gmail.com>
2022-05-15 02:21:08 -0400

    abort launches completed

--- src/hooks/requests.js
+++ src/hooks/requests.js
@@ -30,7 +30,14 @@ async function httpSubmitLaunch(launch) {
 }
 
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  } catch (error) {
+    return { ok: false };
+  }
+
   // Delete launch with given ID.
 }
 
